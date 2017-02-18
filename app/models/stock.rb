7cc1ec25eabbc5a89f49e93b9667c35ab448b6f7
sagar-cenation <sagarkeshri@rocketmail.com>
2017-02-18 11:54:03 +0530

    Add stocks and ajax for ui

---
+++ app/models/stock.rb
@@ -0,0 +1,26 @@
+class Stock < ApplicationRecord
+
+	def self.find_by_ticker(ticker_symbol)
+		where(ticker: ticker_symbol).first
+	end
+	
+
+	#class level methods
+	def self.new_from_lookup(ticker_symbol)
+		lookup_up_stock = StockQuote::Stock.quote(ticker_symbol)
+		return nil unless lookup_up_stock.name #return nil for invalid search
+
+		new_stock = new(ticker: lookup_up_stock.symbol, name: lookup_up_stock.name)
+		new_stock.last_price = new_stock.price
+		new_stock
+	end
+
+	def price 
+		closing_price = StockQuote::Stock.quote(ticker).close
+		return "#{closing_price} (Closing)" if closing_price
+
+		opening_price = StockQuote::Stock.quote(ticker).open
+		return "#{opening_price} (Opening)" if opening_price
+		'Unavailable' #otherwise
+	end
+end

0e7d1719ae4c8e702f556517ded7fd4fe95c64a2
sagar-cenation <sagarkeshri@rocketmail.com>
2017-04-03 16:17:21 +0530

    Add friendship and friend search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
   get 'my_friends', to: "users#my_friends"
-  get 'search_friends', to: "users#search"
-  post 'add_friend', to: "users#add_friend" #because we want to create association and add the friend
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend' #because we want to create association and add the friend
 end
